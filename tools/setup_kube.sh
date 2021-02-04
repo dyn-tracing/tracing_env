6395c513a4d18fb067b5199db2d5e954aabaa188
fruffy <5960321+fruffy@users.noreply.github.com>
2021-02-04 17:26:32 -0500

    Renable macos. (#10)
    * Renable macos.
    
    * Docker.
    
    * Shenanigans.
    
    * More stuff.
    
    * last try.
    
    * Minikube is too slow.

--- tools/setup_kube.sh
+++ tools/setup_kube.sh
@@ -13,34 +13,55 @@ sudo apt install -y apt-transport-https gnupg2 curl
 # Fetch the sub modules
 git submodule update --init --recursive
 
+
+if [ "$(uname)" == "Darwin" ]; then
+# Docker
+brew install hyperkit
+brew upgrade hyperkit
+# Kubectl
+brew install kubectl
+# Minikube
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-darwin-amd64
+sudo install minikube-darwin-amd64 /usr/local/bin/minikube
+rm -rf minikube-darwin-amd64
+# Need to use docker because we are in a VM
+minikube config set driver hyperkit
+# Bazel
+/bin/bash -c "$(curl -fsSL \
+https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+brew install bazel
+brew upgrade bazel
+
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
 # Docker
 sudo apt install -y docker.io
 # Docker without sudo
 sudo usermod -aG docker $USER
-
 # Kubectl
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 sudo touch /etc/apt/sources.list.d/kubernetes.list
 echo "deb http://apt.kubernetes.io/ kubernetes-yakkety main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
 sudo apt update
 sudo apt install -y kubectl
-
 # Minikube
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
 sudo dpkg -i minikube_latest_amd64.deb
 rm minikube_latest_amd64.deb
-# Configure minikube
-# Bookinfo requires more memory
-minikube config set memory 4096
 # Need to use docker because we are in a VM
 minikube config set driver docker
-
 # Bazel
 sudo apt install -y curl gnupg
 curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor > bazel.gpg
 sudo mv bazel.gpg /etc/apt/trusted.gpg.d/
 echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
 sudo apt update && sudo apt install bazel
+
+fi
+
+# Configure minikube
+# Bookinfo requires more memory
+minikube config set memory 4096
+
 
 # Need prometheus for the API
 pip3 install --user prometheus-api-client
