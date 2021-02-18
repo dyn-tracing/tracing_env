7538e54354c9305f4f0b5175203846f4b3751881
Khanh Nguyen <Khanhnguyen99hn@gmail.com>
2021-02-18 19:28:41 +0000

    Update based on review

--- kubernetes_env/benchmark/benchmark.py
+++ kubernetes_env/benchmark/benchmark.py
@@ -11,40 +11,41 @@ import matplotlib.pyplot as plt
 import json
 from pathlib import Path
 
-sys.path.append("..")
-
+DIRS = Path(__file__).resolve().parents
+sys.path.append(str(DIRS[1]))
 import kube_env
 import kube_util as util
 
+
 log = logging.getLogger(__name__)
-FILE_DIR = os.path.abspath(os.path.dirname(__file__))
-# Haven't found a workaround
-FORTIO_DIR = Path(__file__).parent.resolve().parent.resolve().parent.resolve()
+FILE_DIR = DIRS[0]
+FILTER_DIR = FILE_DIR.joinpath("cpp_filter")
+GRAPHS_DIR = FILE_DIR.joinpath("graphs")
+DATA_DIR = FILE_DIR.joinpath("data")
+FORTIO_DIR = DIRS[2].joinpath("bin/fortio")
+
 
 def plot(xaxis_data, yaxis_data, xaxis_label, yaxis_label, query, graph_name):
     plt.plot(xaxis_data, yaxis_data, marker='o')
     plt.xlabel(xaxis_label)
     plt.ylabel(yaxis_label)
     plt.title(query)
-    if not check_dir("graphs"):
-      os.mkdir(os.path.join(FILE_DIR, "graphs"))
-    plt.savefig(os.path.join(FILE_DIR, f"graphs/{graph_name}.png"))
+    util.check_dir(GRAPHS_DIR)
+    plt.savefig(str(GRAPHS_DIR.joinpath(f"{graph_name}.png")))
 
-def check_dir(dirname):
-    return os.path.isdir(os.path.join(FILE_DIR, dirname))
-
-def make_dir(dirname):
-    os.mkdir(os.path.join(FILE_DIR, dirname))
 
 def run_fortio(platform, threads, qps, run_time):
-    if not check_dir("data"):
-      make_dir("data")
+    util.check_dir(DATA_DIR)
     _, _, gateway_url = kube_env.get_gateway_info(platform)
-    cmd = f"{FORTIO_DIR}/bin/fortio "
-    cmd += f"load -c {threads} -qps {qps} -jitter -t {run_time}s -json data/output.json "
+    output_file = str(DATA_DIR.joinpath("output.json"))
+    fortio_dir = str(FORTIO_DIR)
+    cmd = f"{fortio_dir} "
+    cmd += f"load -c {threads} -qps {qps} -jitter -t {run_time}s -json {output_file} "
     cmd += f"http://{gateway_url}/productpage"
-    fortio_proc = util.start_process(cmd, preexec_fn=os.setsid)
-    outs, errs = fortio_proc.communicate()
+    with open(output_file, "w") as f:
+      fortio_res = util.exec_process(cmd, 
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+      return fortio_res
 
 def get_histogram(file_name):
     with open(f"data/{file_name}", "r") as f:
@@ -53,7 +54,9 @@ def get_histogram(file_name):
        return data
 
 def benchmark_filter(platform, threads, qps, time, file_name, graph_name):
-    run_fortio(platform, threads, qps, time)
+    fortio_res = run_fortio(platform, threads, qps, time)
+    if fortio_res == util.EXIT_FAILURE:
+      return fortio_res
     data = get_histogram(file_name)
     if data is not None:
       latency_data = data["Data"]
@@ -61,41 +64,45 @@ def benchmark_filter(platform, threads, qps, time, file_name, graph_name):
       request_counts = [resp["Count"] for resp in latency_data]
       plot(response_times, request_counts, "Response time (ms)", 
            "Requests", f"Platform: {platform} Threads: {threads} QPS: {qps}", graph_name)
+      return util.EXIT_SUCCESS
     else:
       log.error("Error in processing data")
+      return util.EXIT_FAILURE
 
 def start_benchmark(filter_dirs, platform, 
                     threads, qps, time, file_name, graph_name):
     if kube_env.check_kubernetes_status() != util.EXIT_SUCCESS:
       log.error("Kubernetes is not set up."
                   " Did you run the deployment script?")
-      sys.exit(util.EXIT_FAILURE)
+      return util.EXIT_FAILURE
 
-    # Undeploy current running filter
-    kube_env.undeploy_filter() 
     for (idx, filter_dir) in enumerate(filter_dirs):
       build_res = kube_env.build_filter(filter_dir)
 
       if build_res != util.EXIT_SUCCESS:
         log.error(f"Setting filter failed for {filter_dir}." 
                     " Make sure you give the right path")
-        sys.exit(util.EXIT_FAILURE)
+        return util.EXIT_FAILURE
 
       filter_res = kube_env.deploy_filter(filter_dir) 
       if filter_res != util.EXIT_SUCCESS:
         log.error(f"Setting filter failed for {filter_dir}." 
                     " Make sure you give the right path")
-        sys.exit(util.EXIT_FAILURE)
+        return util.EXIT_FAILURE
       
-      benchmark_filter(platform, threads, qps, time,
+      benchmark_res = benchmark_filter(platform, threads, qps, time,
                        file_name, f"{graph_name}{idx}")
+      if benchmark_res != util.EXIT_SUCCESS:
+        log.error(f"Error benchmarking for {filter_dir}")
+        return util.EXIT_FAILURE
+
       undeploy_res = kube_env.undeploy_filter()
       if undeploy_res != util.EXIT_SUCCESS:
         log.error(f"Setting filter failed for {filter_dir}." 
                     " Make sure you give the right path")
-        sys.exit(util.EXIT_FAILURE)
-      
-
+        return util.EXIT_FAILURE
+    return util.EXIT_SUCCESS
+    
 
 def main(args):
     filter_dirs = args.filter_dirs
@@ -106,7 +113,7 @@ def main(args):
     file_name = args.file_name
     graph_name = args.graph_name
 
-    start_benchmark(filter_dirs, platform, threads,
+    return start_benchmark(filter_dirs, platform, threads,
                     qps, time, file_name, graph_name)
     
 if __name__ == '__main__':
@@ -130,7 +137,7 @@ if __name__ == '__main__':
                         " do you want a multi-zone cluster?")                      
     parser.add_argument("-fds", "--filter-dirs", dest="filter_dirs",
                         nargs="+", type=str,
-                        default="./cpp_filter",
+                        default=[str(FILTER_DIR)],
                         help="List of directories of the filter")
     parser.add_argument("-th", "--threads", dest="threads",
                         type=int, default=50,
