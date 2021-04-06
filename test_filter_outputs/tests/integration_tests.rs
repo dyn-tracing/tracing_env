31d992db9e6c32d517f6dc92862740afb7715e31
fruffy <fruffy@nyu.edu>
2021-04-06 16:44:38 +0200

    Fixes.

--- test_filter_outputs/tests/integration_tests.rs
+++ test_filter_outputs/tests/integration_tests.rs
@@ -86,56 +86,56 @@ mod tests {
         "productpage-v1\n" ,
         None, false ; "service_name_test"
     )]
-    // #[test_case(
-    //     "service_name_distributed",
-    //     "get_service_name.cql",
-    //     vec![],
-    //     "productpage-v1\n" ,
-    //     None, true ; "service_name_distributed_test"
-    // )]
-    // #[test_case(
-    //     "height",
-    //     "height.cql",
-    //     vec!["height.rs"],
-    //     "2\n",
-    //     None , false ; "height_test"
-    // )]
-    // #[test_case(
-    //     "height_distributed",
-    //     "height.cql",
-    //     vec!["height.rs"],
-    //     "2\n",
-    //     None , true ; "height_distributed_test"
-    // )]
-    // #[test_case(
-    //     "request_size_avg",
-    //     "request_size_avg.cql",
-    //     vec![],
-    //     "1",
-    //     Some("../tracing_sim/target/debug/libaggregation_example"), false ; "request_size_avg_test"
-    // )]
-    // #[test_case(
-    //     "request_size_avg_distributed",
-    //     "request_size_avg.cql",
-    //     vec![],
-    //     "1",
-    //     Some("../tracing_sim/target/debug/libaggregation_example"), true ; "request_size_avg_distributed_test"
-    // )]
+    #[test_case(
+        "service_name_distributed",
+        "get_service_name.cql",
+        vec![],
+        "productpage-v1\n" ,
+        None, true ; "service_name_distributed_test"
+    )]
+    #[test_case(
+        "height",
+        "height.cql",
+        vec!["height.rs"],
+        "2\n",
+        None , false ; "height_test"
+    )]
+    #[test_case(
+        "height_distributed",
+        "height.cql",
+        vec!["height.rs"],
+        "2\n",
+        None , true ; "height_distributed_test"
+    )]
+    #[test_case(
+        "request_size_avg",
+        "request_size_avg.cql",
+        vec![],
+        "1",
+        Some("../tracing_sim/target/debug/libaggregation_example"), false ; "request_size_avg_test"
+    )]
+    #[test_case(
+        "request_size_avg_distributed",
+        "request_size_avg.cql",
+        vec![],
+        "1",
+        Some("../tracing_sim/target/debug/libaggregation_example"), true ; "request_size_avg_distributed_test"
+    )]
 
-    // #[test_case(
-    //     "request_size_avg_trace_attr",
-    //     "request_size_avg_trace_attr.cql",
-    //     vec![],
-    //     "1",
-    //     Some("../tracing_sim/target/debug/libaggregation_example"), false ; "request_size_avg_trace_attr_test"
-    // )]
-    // #[test_case(
-    //     "request_size_avg_trace_attr_distributed",
-    //     "request_size_avg_trace_attr.cql",
-    //     vec![],
-    //     "1",
-    //     Some("../tracing_sim/target/debug/libaggregation_example"), true ; "request_size_avg_trace_attr_distributed_test"
-    // )]
+    #[test_case(
+        "request_size_avg_trace_attr",
+        "request_size_avg_trace_attr.cql",
+        vec![],
+        "1",
+        Some("../tracing_sim/target/debug/libaggregation_example"), false ; "request_size_avg_trace_attr_test"
+    )]
+    #[test_case(
+        "request_size_avg_trace_attr_distributed",
+        "request_size_avg_trace_attr.cql",
+        vec![],
+        "1",
+        Some("../tracing_sim/target/debug/libaggregation_example"), true ; "request_size_avg_trace_attr_distributed_test"
+    )]
 
     fn test(
         query_id: &str,
