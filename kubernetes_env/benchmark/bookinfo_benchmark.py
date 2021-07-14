c140b62244a30cc0ca3fb2345a20d0e68039d792
Jessica Berg <jesswberg@gmail.com>
2021-07-14 10:42:35 -0400

    bookinfo locust file now outputs raw data correctly

--- kubernetes_env/benchmark/bookinfo_benchmark.py
+++ kubernetes_env/benchmark/bookinfo_benchmark.py
@@ -1,10 +1,11 @@
 from locust import HttpUser, task, between, events
 import locust
 import time
+import csv
 
 wait_time = 1
 start_time = 0
-stats = {}
+stats = []
 
 class BookInfoUser(HttpUser):
     wait_time = between(1, 2)
@@ -18,9 +19,17 @@ def on_request(request_type, name, response_time, response_length, response,
                    context, exception, **kwargs):
     if start_time != 0 and time.time() > start_time:
         request_start_time = context["request_start_time"]
-        print(f"{request_start_time}, {name}, {response_time}, {response.status_code}")
+        stats.append([request_start_time, name, response_time, response.status_code])
 
 @events.spawning_complete.add_listener
 def set_recording_time(user_count, **kwargs):
     global start_time
     start_time = time.time() + wait_time # start recording stats at wait_time after Locust is fully running
+
+@events.quitting.add_listener
+def write_to_csv(environment, **kwargs):
+    with open("bookinfo_benchmark_results.csv", "a+") as csvfile:
+        csvwriter = csv.writer(csvfile)
+        for row in stats:
+            csvwriter.writerow(row)
+            
